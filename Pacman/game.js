a12d8ff7589c8b190e905203bac5ed13f150121f
Emo Maat <emosbm@gmail.com>
2019-04-16 17:25:33 +0200

    Updated Pacman to work with the new system

---
+++ Pacman/game.js
@@ -0,0 +1,302 @@
+function PacMan(){
+	new MenuInterface();
+	interfaces.menu.object.buttons = [
+		["GO TO HUB", "loadingBar('hub', 'new HubInterface')"],
+		["HIGH SCORES", "new HighScoresInterface('" + currentGame + "', 0)"],
+		["START GAME", "newPacManGame()"]
+	];
+
+	interfaces.menu.object.backgroundGame = interfaces.game.object = new PacManGame();
+}
+
+function newPacManGame(){	
+	exit_open_game();
+	exit_open_interfaces();
+	
+	interfaces.game.object = new PacManGame();
+	
+	interfaces.game.interval = new Interval(function(){ 
+		interfaces.game.object.update();
+	}, 15);
+}
+
+class PacManGame{
+	constructor(){
+		move.continuous = true;
+		this.window_scale = canvas.height / 1080;
+		this.OverlayIsActiveActivated = false;
+		this.offset = canvas.width / 2 - (900 * canvas.height / 1080) / 2;
+
+		// create a canvas containing the options
+		let PacManCanvas = document.createElement('canvas');
+		PacManCanvas.id = 'PacManCanvas';
+		PacManCanvas.width = canvas.width;
+		PacManCanvas.height = canvas.height;			// is set later
+		PacManCanvas.style.left = 0;
+		PacManCanvas.style.bottom = 0;
+		PacManCanvas.style.position = "absolute";
+		PacManCanvas.style.cursor = "none";
+		PacManCanvas.style.zIndex = 1;
+		document.body.appendChild(PacManCanvas);
+
+		this.canvas = document.getElementById("PacManCanvas");		// canvas stuff
+		this.ctx = this.canvas.getContext("2d");	// canvas stuff
+		
+		this.map = new PacManMap(this.window_scale);
+		this.ai = new PacManAI(this.map.settings, this.map.space, this.map.intersections, this.map.tunnel, this.offset, this.ctx);
+		this.player = new PacManPlayer(this.map.settings, this.map.walls, this.offset, this.ctx);
+
+		this.def_eattimer = 240; 	// 4 x 60 sec
+		this.eattimer = 240;		// if this is below 0, release a ghost
+
+		this.eatables_eaten = 0;
+		this.ghosts_eaten = [];
+
+		// this.sounds = {
+			// chomp: new Audio("Pacman/src/chomp.wav"),
+			// death: new Audio("Pacman/src/death.wav"),
+			// ghosteaten: new Audio("Pacman/src/ghosteat.wav"),
+			// ready: new Audio("Pacman/src/ready.wav"),
+			// bgsound: new Audio("Pacman/src/bgsound.wav")
+		// };
+
+		this.lives = 2;
+		this.level = 1;
+		this.points = 0;
+		
+		this.modeTableHandler();
+		
+		this.ready = {
+			state:false,
+			timer:270,
+			def_timer:270
+		};
+	}
+
+	exit(){
+		document.getElementById("PacManCanvas").parentElement.removeChild(document.getElementById("PacManCanvas"))
+		document.getElementById("PacManMap").parentElement.removeChild(document.getElementById("PacManMap"))
+
+		interfaces.game.interval.stop();
+		interfaces.game.object = {};
+	}
+	
+	update(){
+		this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+		// if in the overlay
+		if(!interfaces.menu.active){
+			// update the game system
+			this.interactionHandler();
+			this.eatablesHandler();
+			this.readyHandler();
+			this.infobarHandler();
+
+			// if not ready, only draw
+			if(this.ready.state){
+				// update the ai and player
+				pacmanBgsound.play();
+				pacmanBgsound.volume = 0.7;
+				pacmanReady.pause();
+
+				this.player.update();
+				this.ai.update({x: this.player.node.x, y: this.player.node.y, angle:this.player.angle}, this.ai.ghosts);
+			} else {
+				pacmanBgsound.pause();
+				pacmanReady.play();
+
+				this.player.mouth_angle = 0.1;
+				this.player.draw();
+				
+				for(var g = 0; g < this.ai.ghosts.length; g++)
+					this.ai.ghosts[g].draw();
+				
+			}
+		} else {
+			if(!this.OverlayIsActiveActivated){
+				this.OverlayIsActiveActivated = true;
+				this.ai.ghosthouse.release(2);
+				this.ai.ghosthouse.release(3);
+				
+				for(var g = 0; g < this.ai.ghosts.length; g++){
+					if(!this.ai.ghosts[g].fleeing)
+						this.ai.ghosts[g].fleeing = true;
+						
+					this.ai.ghosts[g]._animate_fleeing = false;
+				}
+			}
+		
+			this.ai.update({x: this.player.node.x, y: this.player.node.y, angle:this.player.angle}, this.ai.ghosts);
+		}
+	}
+	
+	restoreLevel(){
+		var globalcounter = this.ai.eatTable.counter;
+		this.ghosts_eaten = [];
+		
+		this.player = new PacManPlayer(this.map.settings, this.map.walls, this.offset, this.ctx);
+		this.ai = new PacManAI(this.map.settings, this.map.space, this.map.intersections, this.map.tunnel, this.offset, this.ctx);
+		
+		this.ai.eatTable.counter = globalcounter;
+		this.ai.eatTable.died = true;
+		
+		this.ready.state = false;
+		this.lives--;
+	}
+	
+	levelUp(){
+		this.level++;
+		this.ai.eatTable.died = false;
+		this.eatables_eaten = 0;
+		this.ai.eatTable.counter = 0;
+		this.ready.state = false;
+			
+		this.map = new PacManMap(this.window_scale); 
+		
+		this.modeTableHandler();
+		this.restoreLevel();
+	}
+	
+	readyHandler(){
+		if(this.ready.timer > 0 && !this.ready.state){
+			this.ready.timer--;
+			pacmanBgsound.pause();
+		} else {
+			this.ready.timer = 270;
+			this.ready.state = true;
+		}
+	}
+	
+	infobarHandler(){
+		for(var l = 0; l < this.lives; l++){
+			var img = new Image(); 
+			img.src = "PacMan/src/PacMan.png";
+			this.ctx.drawImage(img, this.map.settings.size * 1.5 * (l + 1), (this.map.size.rows + 2) * this.map.settings.size);
+		}
+
+		this.ctx.font = "30px Arial";
+		this.ctx.fillStyle ="white";
+		this.ctx.fillText("POINTS: " + this.points,this.map.settings.size * 10, (this.map.size.rows + 3) * this.map.settings.size); 
+	}
+
+	modeTableHandler(){
+		for(var m = 0; m < this.ai.speedTable.mode.length; m++)
+			if(this.level >= this.ai.speedTable.mode[m].level_low && this.level <= this.ai.speedTable.mode[m].level_high)
+				this.ai.speedTable.current = m;
+
+		for(var m = 0; m < this.ai.scatterTable.mode.length; m++)
+			if(this.level >= this.ai.scatterTable.mode[m].level_low && this.level <= this.ai.scatterTable.mode[m].level_high)
+				this.ai.scatterTable.current = m;
+		
+		for(var m = 0; m < this.ai.eatTable.mode.length; m++)
+			if(this.level >= this.ai.eatTable.mode[m].level_low && this.level <= this.ai.eatTable.mode[m].level_high)
+				this.ai.eatTable.current = m;
+	}
+	
+	eatablesHandler(){
+		for(var e = 0; e < this.map.eatable.length; e++){
+			if(this.player.node.x == this.map.eatable[e].x && this.player.node.y == this.map.eatable[e].y && !this.map.eatable[e].eaten){
+				this.map.eatable[e].eaten = true;
+				
+				pacmanChomp.currentTime = 0;
+				pacmanChomp.play();
+				
+				if(!this.ai.eatTable.died)
+					this.ai.eatables_eaten++;
+				else
+					this.ai.eatTable.counter++;
+				
+				this.eatables_eaten++;
+				this.points += 10;
+			}
+		}
+
+		for(var p = 0; p < this.map.powerup.length; p++){
+			if(this.player.node.x == this.map.powerup[p].x && this.player.node.y == this.map.powerup[p].y && !this.map.powerup[p].eaten){
+				this.map.powerup[p].eaten = true;
+				this.points += 50;
+				this.ghosts_eaten = [];
+
+				this.player.powered.state = 1;
+				this.player.powered.timer = 600;
+				
+				this.ai.fleeing.state = true;
+				this.ai.fleeing.timer = this.ai.fleeing.deftimer;
+			}
+		}
+		
+		// draw powerups
+		for (var p = 0; p < this.map.powerup.length; p++){
+			if(!this.map.powerup[p].eaten){
+				this.ctx.beginPath(); // if not eaten
+				this.ctx.fillStyle = this.map.settings.powerup.color; 
+			
+				this.ctx.arc((this.map.powerup[p].x + 1) * this.map.settings.size - this.map.settings.size / 2 + this.offset,
+					(this.map.powerup[p].y + 1) * this.map.settings.size - this.map.settings.size / 2,
+					this.map.settings.powerup.size,0,2*Math.PI);
+						
+				this.ctx.fill();
+			}
+		}
+		
+		// draw powerups
+		for (var e = 0; e < this.map.eatable.length; e++){
+			if(!this.map.eatable[e].eaten){ // if not eaten
+				this.ctx.beginPath();
+				this.ctx.fillStyle = this.map.settings.eatable.color; 
+			
+				this.ctx.arc((this.map.eatable[e].x + 1) * this.map.settings.size - this.map.settings.size / 2 + this.offset,
+					(this.map.eatable[e].y + 1) * this.map.settings.size - this.map.settings.size / 2,
+					this.map.settings.eatable.size,0,2*Math.PI);
+						
+				this.ctx.fill();
+			}
+		}	
+	}
+	
+	// check if the player is caught by a ghost
+	interactionHandler(){
+		for(var g = 0; g < this.ai.ghosts.length; g++){
+			// check if the player caught a ghost
+			if (this.ai.ghosts[g].node.x == this.player.node.x && this.ai.ghosts[g].node.y == this.player.node.y && this.player.powered.state != 0 && !this.inArray(g, this.ghosts_eaten)){
+				// update the score accordingly
+				this.ghosts_eaten.push(g);
+				this.ai.ghosthouse.contain(g);	
+
+				this.ai.ghosts[g].eaten = true;
+				this.ai.ghosts[g]._waseaten = true;
+
+				this.points += 400 * this.ghosts_eaten.length;
+
+				//pacmanGhosteaten.play();
+			} else 
+
+			// check if the player is caught
+			if(this.ai.ghosts[g].node.x == this.player.node.x && this.ai.ghosts[g].node.y == this.player.node.y && this.ai.ghosts[g].eaten == false && (this.player.powered.state == 0 || this.inArray(g, this.ghosts_eaten)) || this.player.eaten){
+				
+				pacmanDeath.play();
+
+				this.player.eaten = true;
+				
+				for(var e = 0; e < this.ai.ghosts.length; e++){
+					this.ai.ghosts[e].disablemovement = true;
+				}
+				
+				if(this.player.nodraw && this.lives != 0)
+					this.restoreLevel();
+				else if(this.player.nodraw && this.lives == 0)
+					new HighScoresInterface("Pacman", this.points);
+			} 
+		}
+		
+		if(this.eatables_eaten == 240)
+			this.levelUp();
+	}
+
+	inArray(item, array){
+		for (var i = 0; i < array.length; i++){
+			if(item == array[i])
+				return true;
+		}
+		return false;
+	}
+}
---
+++ Pacman/player.js
@@ -0,0 +1,360 @@
+class PacManPlayer{
+    constructor(wall_settings, wall_map, offset, ctx, size = canvas.width / 128 /* = 15*/){
+        this.x = 14.5;
+        this.y = 24;
+        this.speed = 0.1;
+        this.offset = offset;
+        this.ctx = ctx;
+		
+		this.eaten = false;
+		this.nodraw = false;
+		this.disablemovement = false;
+
+        this.node = {
+            x:14,
+            y:24
+        }
+
+        this.powered = {
+            state: 0,           // 0 = normal 1 = powered 2 = ending
+            size:2,             // pixel in wh will expand
+            timer:600, 			// times it will switch
+            current_size: 0,    // max will be 3 (px)
+            shrinking: false    // switch
+        }
+		
+		this.drawing = [
+			{
+				arc1:1.75,
+				arc2:1.25,
+			},
+			{
+				arc1:0.25,
+				arc2:1.75,
+			},
+			{
+				arc1:0.75,
+				arc2:0.25,
+			},
+			{
+				arc1:1.25,
+				arc2:0.75,
+			},
+			
+			0.75
+		];
+
+        this.mouth_angle = 0.25;    // Math.PI * 0.25 = 45 degrees
+        this.mouth_closing = true;  // switch
+
+        this.angle = 3;             // up = 0, right = 1, down = 2, left = 3 
+        this.radius = size;
+        this.wall_map = wall_map;
+        this.wall_settings = wall_settings;
+    }
+
+    update(){
+        this.updateOwnNode();
+
+		if(!this.disablemovement)
+			this.keyHandler();
+		
+		if(!this.nodraw)
+			this.draw();
+    }
+
+    keyHandler(){
+        if(move.right || this.angle == 1){
+            if(!this.isWall(this.speed, 0)){ // is it a wall?
+                if(this.x + this.speed > this.node.x + 1){ // are we going past a node
+                    this.x = this.node.x + 1; // make our location the node
+                } else if(this.y == Math.round(this.y)){ // so we can move in an other direction
+                    this.x += this.speed;
+                    this.angle = 1;
+                }
+            } else {
+                this.x = this.node.x;
+            }
+        }
+
+        if(move.left || this.angle == 3){
+            if(!this.isWall(-this.speed, 0)){
+                if(this.x - this.speed < this.node.x - 1){
+                    this.x = this.node.x - 1;
+                } else if(this.y == Math.round(this.y)){
+                    this.x -= this.speed;
+                    this.angle = 3;
+                }
+            } else {
+                this.x = this.node.x;
+            }
+        }
+
+        // remove noise after four digits
+        this.x = Math.round(this.x * 1000) / 1000;
+
+        if(move.down || this.angle == 2){
+            if(!this.isWall(0, this.speed)){
+                if(this.y + this.speed > this.node.y + 1){
+                    this.y = this.node.y + 1;
+                } else if(this.x == Math.round(this.x)){
+                    this.y += this.speed;
+                    this.angle = 2;
+                }
+            } else {
+                this.y = this.node.y;
+            }
+        }
+
+        if(move.up || this.angle == 0){
+            if(!this.isWall(0, -this.speed)){
+                if(this.y - this.speed < this.node.y - 1){
+                    this.y = this.node.y - 1;
+                } else if(this.x == Math.round(this.x)){
+                    this.y -= this.speed;
+                    this.angle = 0;
+                }
+            } else {
+                this.y = this.node.y;
+            }
+        }
+
+        // remove noise after four digits
+        this.y = Math.round(this.y * 1000) / 1000;
+
+        // teleport part
+        if(this.node.x == 28 && this.node.y == 15){
+            this.x = 2; this.node.x = 2;} 
+        else if(this.node.x == 1 && this.node.y == 15){
+            this.x = 27; this.node.x = 27;}
+    }
+
+    draw(){
+        this.ctx.fillStyle="black";
+        if (this.powered.state == 1){ // go to powered state
+            this.ctx.lineWidth = this.wall_settings.border_thickness - 2 + this.powered.current_size;
+            if(this.powered.current_size < this.powered.size) // expand till 1.5
+                this.powered.current_size += 0.125;
+			
+			if(this.powered.timer < 600 * 0.3)
+				this.powered.state = 2;
+				
+			this.powered.timer--;
+
+        } else if (this.powered.state == 2){ // switch between 0 and 1
+            this.ctx.lineWidth = this.wall_settings.border_thickness - 2 + this.powered.current_size;
+
+            // flicker
+            if(this.powered.shrinking){
+                this.powered.current_size -= 0.125;
+                if (this.powered.current_size <= 0){
+                    this.powered.current_size = 0;
+                    this.powered.shrinking = false;
+                }
+            } else {
+                this.powered.current_size += 0.125;
+                if (this.powered.current_size >= this.powered.size){
+                    this.powered.current_size = this.powered.size;
+                    this.powered.shrinking = true;
+                }
+            }
+			
+			this.powered.timer--;
+
+            if(this.powered.timer <= 0){
+                this.powered.state = 0;     // set the state to default
+                this.powered.timer = 600;    // reset the timer for the next activation
+            }                 
+
+        } else { // default == not in powered state
+            this.ctx.lineWidth = this.wall_settings.border_thickness - 2 + this.powered.current_size;
+            if(this.powered.current_size > 0) // shrink till 0
+                this.powered.current_size -= 0.125;
+        }
+
+		var x = this.x + 0.5 + this.offset / this.wall_settings.size;	// position correction
+		var y = this.y + 0.5;	// position correction
+
+        this.ctx.beginPath();
+
+        // move to the center of pacman
+        this.ctx.moveTo(x * this.wall_settings.size, y * this.wall_settings.size);
+
+        this.ctx.strokeStyle="white";
+        this.ctx.beginPath();
+        // then we draw the outline border
+		
+		// if eaten, pacman should disappear
+		if(this.eaten){
+			this.disablemovement = true;
+			if(this.mouth_angle > 0) {
+					this.mouth_angle -= 0.01;
+			} else {	
+				// as all arcs need to either be + or - 0.75 from its original value, it is ok to only check the first one
+				if(this.drawing[this.drawing.length - 1] > 0.01){
+					// skip the last one for it is the counter
+					for(var d = 0; d < this.drawing.length - 1; d++){
+						this.drawing[d].arc1 += 0.01;
+						this.drawing[d].arc2 -= 0.01;
+					}
+					this.drawing[this.drawing.length - 1] -= 0.01;
+					
+				} else {
+					this.nodraw = true;
+				}
+			}
+		}
+
+        // draw the border and a line to the middle
+        if(this.angle == 0) {
+            // draw the circle
+            this.ctx.arc(x * this.wall_settings.size, y * this.wall_settings.size, this.radius, Math.PI * (this.drawing[0].arc1 - this.mouth_angle), Math.PI * (this.drawing[0].arc2 + this.mouth_angle), false); // up
+            
+            // 0.08 = graphical correction
+            // calculate the position of the gap of the circle
+            var x0 = x * this.wall_settings.size - 0.8 + this.radius * -Math.cos((this.drawing[0].arc1 - this.mouth_angle) * Math.PI);
+            var y0 = y * this.wall_settings.size + 0.8 + this.radius * Math.sin((this.drawing[0].arc2 + this.mouth_angle) * Math.PI);  
+
+            var x1 = x * this.wall_settings.size + 0.8 + this.radius * Math.cos((this.drawing[0].arc1 - this.mouth_angle) * Math.PI);
+            var y1 = y * this.wall_settings.size + 0.8 + this.radius * Math.sin((this.drawing[0].arc2 + this.mouth_angle) * Math.PI);  
+
+            // draw the lines
+            this.ctx.moveTo(x0, y0);
+            this.ctx.lineTo(x * this.wall_settings.size, y * this.wall_settings.size);
+
+            this.ctx.moveTo(x1, y1);
+            this.ctx.lineTo(x * this.wall_settings.size, y * this.wall_settings.size);
+			
+        } else if(this.angle == 1) {
+            this.ctx.arc(x * this.wall_settings.size, y * this.wall_settings.size, this.radius, Math.PI * (this.drawing[1].arc1 - this.mouth_angle), Math.PI * (this.drawing[1].arc2 + this.mouth_angle), false); // right
+        
+            // 0.08 = graphical correction
+            // calculate the position of the gap of the circle
+            var x0 = x * this.wall_settings.size - 0.8 + this.radius * Math.cos((this.drawing[1].arc1 - this.mouth_angle) * Math.PI);
+            var y0 = y * this.wall_settings.size + 0.8 + this.radius * -Math.sin((this.drawing[1].arc2 + this.mouth_angle) * Math.PI);  
+
+            var x1 = x * this.wall_settings.size + 0.8 + this.radius * Math.cos((this.drawing[1].arc1 - this.mouth_angle) * Math.PI);
+            var y1 = y * this.wall_settings.size - 0.8 + this.radius * Math.sin((this.drawing[1].arc2 + this.mouth_angle) * Math.PI);  
+
+            // draw the lines
+            this.ctx.moveTo(x0, y0);
+            this.ctx.lineTo(x * this.wall_settings.size, y * this.wall_settings.size);
+
+            this.ctx.moveTo(x1, y1);
+            this.ctx.lineTo(x * this.wall_settings.size, y * this.wall_settings.size);
+
+        } else if(this.angle == 2) {
+            this.ctx.arc(x * this.wall_settings.size, y * this.wall_settings.size, this.radius, Math.PI * (this.drawing[2].arc1 - this.mouth_angle), Math.PI * (this.drawing[2].arc2 + this.mouth_angle), false); // down
+        
+            // 0.08 = graphical correction
+            // calculate the position of the gap of the circle
+            var x0 = x * this.wall_settings.size + 0.8 + this.radius * -Math.cos((this.drawing[2].arc1 - this.mouth_angle) * Math.PI);
+            var y0 = y * this.wall_settings.size + 0.8 + this.radius * Math.sin((this.drawing[2].arc2 + this.mouth_angle) * Math.PI);  
+
+            var x1 = x * this.wall_settings.size - 0.8 + this.radius * Math.cos((this.drawing[2].arc1 - this.mouth_angle) * Math.PI);
+            var y1 = y * this.wall_settings.size + 0.8 + this.radius * Math.sin((this.drawing[2].arc2 + this.mouth_angle) * Math.PI); 
+
+            // draw the lines
+            this.ctx.moveTo(x0, y0);
+            this.ctx.lineTo(x * this.wall_settings.size, y * this.wall_settings.size);
+
+            this.ctx.moveTo(x1, y1);
+            this.ctx.lineTo(x * this.wall_settings.size, y * this.wall_settings.size);
+
+        } else {
+            this.ctx.arc(x * this.wall_settings.size, y * this.wall_settings.size, this.radius, Math.PI * (this.drawing[3].arc1 - this.mouth_angle), Math.PI * (this.drawing[3].arc2 + this.mouth_angle), false); // left
+        
+            // 0.08 = graphical correction
+            // calculate the position of the gap of the circle
+            var x0 = x * this.wall_settings.size + 0.8 + this.radius * Math.cos((this.drawing[3].arc1 - this.mouth_angle) * Math.PI);
+            var y0 = y * this.wall_settings.size - 0.8 + this.radius * -Math.sin((this.drawing[3].arc2 + this.mouth_angle) * Math.PI);  
+
+            var x1 = x * this.wall_settings.size - 0.8 + this.radius * Math.cos((this.drawing[3].arc1 - this.mouth_angle) * Math.PI);
+            var y1 = y * this.wall_settings.size + 0.8 + this.radius * Math.sin((this.drawing[3].arc2 + this.mouth_angle) * Math.PI);  
+
+            // draw the lines
+            this.ctx.moveTo(x0, y0);
+            this.ctx.lineTo(x * this.wall_settings.size, y * this.wall_settings.size);
+
+            this.ctx.moveTo(x1, y1);
+            this.ctx.lineTo(x * this.wall_settings.size, y * this.wall_settings.size);
+        }
+
+        this.ctx.stroke();
+
+		if(!this.eaten){
+			// this animates the mouth movement
+			var closingSpeed = 0.068; // mouth movement speed, must be equal or below 0.25
+			if(this.mouth_closing){
+				this.mouth_angle -= closingSpeed;
+				if (this.mouth_angle <= 0){
+					this.mouth_angle = 0;
+					this.mouth_closing = false;
+				}
+			} else {
+				this.mouth_angle += closingSpeed;
+				if (this.mouth_angle >= 0.25){
+					this.mouth_angle = 0.2499; // making it larger than 0.25 results in a flicker as it can't draw the circle
+					this.mouth_closing = true;
+				}
+			}        
+		}
+    }
+    /**
+     * checks if movement is possible. One variable must be 0 
+     * @param {direction} x either positive, negative or 0
+     * @param {direction} y either positive, negative or 0
+     */
+    isWall(x = 0, y = 0){
+        if(x == 0 && y == 0){
+            return false;
+        }
+
+        for (let i = 0; i < this.wall_map.length; i++){
+            // if x is positive
+			if(x > 0){
+                if (y == 0){
+                    if(Math.ceil(this.x + x) == this.wall_map[i].x && Math.round(this.y) == this.wall_map[i].y){
+                        return true;
+                    }
+                }
+            } 
+            // if x is negative
+            else if(x < 0){
+                if (y == 0){
+                    if(Math.floor(this.x + x) == this.wall_map[i].x && Math.round(this.y) == this.wall_map[i].y){
+                        return true;
+                    }
+                }
+            } 
+            // if x is 0
+            else if (x == 0){
+                if(y > 0){
+                    if(Math.floor(this.x) == this.wall_map[i].x && Math.ceil(this.y + y) == this.wall_map[i].y){
+                        return true;
+                    }
+                } else if (y < 0){
+                    if(Math.floor(this.x) == this.wall_map[i].x && Math.floor(this.y + y) == this.wall_map[i].y){
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    updateOwnNode(){
+        if(this.x + this.speed > this.node.x + 1)
+            this.node.x += 1;
+
+        if(this.x - this.speed < this.node.x - 1)
+            this.node.x -= 1;
+
+        if(this.y + this.speed > this.node.y + 1)
+            this.node.y += 1;
+
+        if(this.y - this.speed < this.node.y - 1)
+            this.node.y -= 1;
+    }
+}
